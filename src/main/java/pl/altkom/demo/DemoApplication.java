04c91af6c60d735fb43229018b124e384defa2b3
mpietrewicz <mpietrewicz@gmail.com>
2022-07-20 10:29:54 +0200

    Zwracanie listy studentów przez get

--- src/main/java/pl/altkom/demo/DemoApplication.java
+++ src/main/java/pl/altkom/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.altkom.demo.student.Student;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,10 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> getStudent() {
+		Student studentAnna = new Student(LocalDate.parse("1999-01-01"), "Anna", "anna@gmail.com", 23);
+		Student studentJan = new Student(LocalDate.parse("1991-09-22"), "Jan", "Jan@gmail.com", 31);
+		return List.of(studentAnna, studentJan);
 	}
 
 }
