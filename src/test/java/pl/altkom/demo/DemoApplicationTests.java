2019da56e17c18a85c0f1e7ecef6c04b23c1abd0
mpietrewicz <mpietrewicz@gmail.com>
2022-07-20 10:05:54 +0200

    inicjacja

---
+++ src/test/java/pl/altkom/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.altkom.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
